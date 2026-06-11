cef49d7675c053ab361b667037a42a6e3ed9009d
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-06-11 10:57:10 +0700

    BinarySearchTree 8

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -74,4 +74,20 @@ public:
             else
                 currentNode = currentNode->rightchild;
         } 
-    }    
+    } 
+    
+    void inorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
