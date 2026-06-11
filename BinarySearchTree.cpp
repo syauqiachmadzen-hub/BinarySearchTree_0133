0dd104ad0b238155fe2000e406f69e27170f933b
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-06-11 11:02:35 +0700

    BinarySearchTree 11

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -131,5 +131,16 @@ public:
 int main()
 {
     BinaryTree obj;
+
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. implement insert operation" << endl;
+        cout << "2. perform inorder traversal" << endl;
+        cout << "3. perform preorder traversal" << endl;
+        cout << "4. perform postorder traversal" << endl;
+        cout << "5. exit" << endl;
+        cout << "\nEnter your choice (1-5): ";
+    }    
 }    
 
